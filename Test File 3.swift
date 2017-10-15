f2b42e205cbd48d7c0f0b8edfc2abf7bdb222bd0
Marty09 <scott.shadday@gmail.com>
2017-10-15 18:00:59 -0400

    Test SWIFT code file
    Made using Notepad.  I have been unable to establish a working environment to create Iosmac files on my pc.

---
+++ Test File 3.swift
@@ -0,0 +1,41 @@
+func Line1() -> String 
+{
+	return "Three rings to the Elven-kings who live beneath the sky,"
+}
+func Line2() -> String 
+{
+	return "Seven rings to the Dwarf-lords in their halls of stone,"
+}
+func Line3() -> String 
+{
+	return "Nine rings for Mortal Men, doomed to die,"
+}
+func Line4() -> String 
+{
+	return "One for the Dark Lord on his dark throne,"
+}
+func Line5() -> String 
+{
+	return "In the land of Mordor where shadows lie."
+}
+func Line6() -> String 
+{
+	return "One Ring to rule them all, One ring to find them,"
+}
+func Line7() -> String 
+{
+	return "One Ring to bring them all and in the darkness bind them,"
+}
+func Line8() -> String 
+{
+	return "In the land of Mordor where shadows lie."
+}
+print(Line1())
+print(Line2())
+print(Line3())
+print(Line4())
+print(Line5())
+print(Line6())
+print(Line7())
+print(Line8())
+
